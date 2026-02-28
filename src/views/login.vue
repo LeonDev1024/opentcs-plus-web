57eab035ddc3c3c8fe6f468787f87983b4282690
liuyichun <liu_yichun@163.com>
2026-02-28 17:43:38 +0800

    优化标题

--- src/views/login.vue
+++ src/views/login.vue
@@ -54,7 +54,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2025 lyc All Rights Reserved.</span>
+      <span>Copyright © 2026 lyc All Rights Reserved.</span>
     </div>
   </div>
 </template>
