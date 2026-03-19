1874cc19a42e39e3cf7828990fbab1ef4323e8c2
liuyichun <liu_yichun@163.com>
2026-03-19 10:43:38 +0800

    chore: 更新 .gitignore 配置，添加 CLAUDE.md 项目说明
    - 添加 .vite/ 构建缓存目录到 .gitignore
    - 添加 .github/ 工作流配置目录到 .gitignore
    - 添加 CLAUDE.md 项目说明文件
    - 优化 notice 组件代码

--- src/layout/components/notice/index.vue
+++ src/layout/components/notice/index.vue
@@ -19,7 +19,7 @@
       </template>
       <el-empty v-else :description="'消息为空'"></el-empty>
     </div>
-    <div v-if="newsList.length > 0" class="foot-box" @click="onGoToGiteeClick">前往gitee</div>
+    <div v-if="newsList.length > 0" class="foot-box" @click="onGoToGithubClick">前往github</div>
   </div>
 </template>
 
@@ -53,8 +53,8 @@ const onNewsClick = (item: any) => {
 };
 
 // 前往通知中心点击
-const onGoToGiteeClick = () => {
-  window.open('https://gitee.com/dromara/RuoYi-Vue-Plus/tree/5.X/');
+const onGoToGithubClick = () => {
+  window.open('https://github.com/LeonDev1024/opentcs-plus');
 };
 
 onMounted(() => {
