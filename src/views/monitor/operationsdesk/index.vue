a0ce045fffba5d53fc2d63905c49efde2d0c809c
liuyichun <liu_yichun@163.com>
2026-07-28 20:27:42 +0800

    fix(monitor):场景监控

--- src/views/monitor/operationsdesk/index.vue
+++ src/views/monitor/operationsdesk/index.vue
@@ -132,7 +132,7 @@ onMounted(async () => {
       <div class="top-bar">
         <div class="top-bar-left">
           <!-- 工厂下拉选择，默认第一个 -->
-          <span class="factory-label">地图监控：</span>
+          <span class="factory-label">场景监控：</span>
           <el-select
             v-model="currentFactoryId"
             placeholder="请选择工厂"
